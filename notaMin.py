ffa2a0af5f9da0054ad7faffadcc0ed62ce02f10
Gabriel Meruzzi <97992015+Meruzzi@users.noreply.github.com>
2023-05-30 04:29:23 -0300

    Add files via upload
    - Adicionado verificações das respostas do usuário.
    - Adicionado comentários para melhor entendimento do código.
    - Verificacação da nota minima dos candidatos de 1 a 10 e números de vezes que o usuário deseja verificar as notas precisa ser maior que 0.
    -

--- notaMin.py
+++ notaMin.py
@@ -1,3 +1,5 @@
+
+#Array com os candidatos disponiveis para verificar as notas
 candidatos = [
     ('Candidato um',5,10,8,8),
     ('Candidato dois',10,7,7,8),
@@ -5,6 +7,8 @@ candidatos = [
     ('Candidato quatro',2,2,2,1),
     ('Candidato cinco',10,10,8,9)
 ]
+
+#Função para verificar as notas dos candidatos disponiveis na Array e retornar em uma lista os usuarios qualificados.
 def verificarCanditados(notaMin1, notaMin2, notaMin3, notaMin4):
     listaCanditadosSelecionados = []
     for candidato in candidatos:
@@ -16,23 +20,53 @@ def verificarCanditados(notaMin1, notaMin2, notaMin3, notaMin4):
         if nota1 >= notaMin1 and nota2 >= notaMin2 and nota3 >= notaMin3 and nota4 >= notaMin4:
             listaCanditadosSelecionados.append(nomeCandidato)
     return listaCanditadosSelecionados
+
+#Função com o inicio do programa, perguntando quantas vezes o usuario deseja fazer a verificação de notas e pergunta a nota minima desejada.
 def inicioPrograma():
     print("Ola, seja bem vindo!")
-    qntVezes = int(input("Quantas vezes deseja verificar a nota dos candidatos? "))
+
+    #Verifica se o número é acima de 0.
+    while True:
+        try:
+            qntVezes = int(input("Quantas vezes deseja verificar a nota dos candidatos? "))
+            if qntVezes > 0:
+                break
+            else:
+                print("A quantidade de vezes deve ser acima de 0.")
+        except ValueError:
+            print("Informe apenas números.")
+    
     for i in range(qntVezes):
         if i == 0:
             print("Informe as notas mínimas:")
         else:
             print(f"Informe as notas mínimas do {i+1} candidato:")
-        notaMin1 = int(input("Nota minima 1: "))
-        notaMin2 = int(input("Nota minima 2: "))
-        notaMin3 = int(input("Nota minima 3: "))
-        notaMin4 = int(input("Nota minima 4: "))
+        
+        #Verifica se as respostas foram números validos
+        while True:
+            try:
+                notaMin1 = int(input("Nota minima 1: "))
+                notaMin2 = int(input("Nota minima 2: "))
+                notaMin3 = int(input("Nota minima 3: "))
+                notaMin4 = int(input("Nota minima 4: "))
+                break
+            except ValueError:
+                print("Por favor, informe apenas números.")
+        
+        #Verifica se as notas estao entre 1 e 10
+        while not (1 <= notaMin1 <= 10 and 1 <= notaMin2 <= 10 and 1 <= notaMin3 <= 10 and 1 <= notaMin4 <= 10):
+            print("As notas mínimas devem estar entre 1 e 10.")
+            notaMin1 = int(input("Nota minima 1: "))
+            notaMin2 = int(input("Nota minima 2: "))
+            notaMin3 = int(input("Nota minima 3: "))
+            notaMin4 = int(input("Nota minima 4: "))
+
         listaCanditadosSelecionados = verificarCanditados(notaMin1, notaMin2, notaMin3, notaMin4)
         if len(listaCanditadosSelecionados) > 0:
             print("Candidatos que possuem a nota minima informada:")
             for candidato in listaCanditadosSelecionados:
                 print(candidato)
         else:
-            print("Nenhum canditado conseguiu a nota minima.")
+            print("Nenhum candidato conseguiu a nota minima.")
+
 inicioPrograma()
